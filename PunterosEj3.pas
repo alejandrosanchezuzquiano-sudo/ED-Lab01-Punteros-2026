56271c0061d719d4b35ed504ca7cfb075a59e30d
Alex <alejandrosanchezuzquiano@gmail.com>
2026-02-05 12:45:00 +0100

    SolucionEj3

--- PunterosEj3.pas
+++ PunterosEj3.pas
@@ -1,30 +1,36 @@
 program PunterosEj3;
 type
     { Declara un tipo de registro llamado “nodo” con dos campos: un entero y un puntero a “nodo” }
+    p = ^integer;
+    TNodo = record
+      nodo : integer;
+      p_nodo : ^Tnodo;
+    end;
 
 
 var
-    { Declara una variable de tipo puntero a nodo }
+    P_Reg : ^TNodo;
+    TRegistro : TNodo;
 
 begin
     { Crea un nodo en memoria dinámica (heap) }
-
+    new(P_Reg);
 
     { Dale valor 100 al campo entero de este nodo }
-
+    P_reg^.nodo := 100;
 
     { Pon a nil el campo puntero del nodo }
-
+    P_Reg := nil;
 
     { Pon ahora el puntero del nodo a apuntar al propio nodo }
-
+    P_reg^.p_nodo := P_reg;
 
     { En este punto hay dos instrucciones alternativas para liberar la memoria dinámica creada: }
     { Alternativa 1: Liberar el nodo directamente }
-
+    dispose(P_Reg);
 
     { Alternativa 2: Liberar el nodo a través del campo puntero }
     { Dispose(punteroNodo^.siguiente); }
-
+    dispose(TRegistro.p_nodo);
     { Termina }
 end.
