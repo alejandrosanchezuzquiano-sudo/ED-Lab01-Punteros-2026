c691dee9ad3d29fbc968bbcb3b85c3b6289fd2a3
Alex <alejandrosanchezuzquiano@gmail.com>
2026-02-05 11:45:49 +0100

    SolucionEj1

--- PunterosEj1.pas
+++ PunterosEj1.pas
@@ -8,35 +8,35 @@ uses
     crt;
 
 var
-    { Declara variable entera (x) }
-    { Declara una variable de tipo puntero a entero (p_ent) }
+    x : integer;
+    P_ent : ^integer;
 
 begin
 
-    { Da el valor 100 a x }
+    x := 100;
 
-    { Crea un entero dinámicamente con p_ent y dale el valor que tiene actualmente x }
+    new(P_ent);
 
-    { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
+    writeln(P_ent^);
 
-    { Crea con new un nuevo entero dinámicamente }
+    new(P_ent);
 
-    { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
+    writeln(P_ent^);
 
-    { Pon el puntero p_ent a apuntar a x }
+    P_ent := @x;
 
-    { Súmale 100 al entero apuntado por el puntero p_ent }
+    P_ent^ := P_ent^ + 100;
 
-    { Imprime por pantalla el valor de x y también del entero al que apunta p_ent }
+    writeln('Valor de x: ',x,'Valor de P_ent: ',P_ent^);
 
-    { Pon el puntero p_ent a NIL }
+    P_ent := nil;
 
-    { Libera la memoria asociada al nuevo entero }
+    dispose(P_ent);
 
-    { Suma 100 a x pero sin usar x en la operación de suma (solo usando p_ent) }
+    P_ent^ := P_ent^ + 100;
 
-    { Muestra por pantalla que son iguales }
+    writeln(P_ent^,' = ',x);
 
-    { Libera toda la memoria asociada a p_ent y termina }
+    dispose(P_ent);
 
 end.
